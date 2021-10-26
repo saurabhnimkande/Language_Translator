d031b035090bcdf7e6bdd805f397a57c7a6da204
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-10-26 21:14:28 +0530

    added text to speech out /in

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,58 +1,75 @@
-async function getLanguages() {
-    let res =await fetch('https://libretranslate.de/languages');
-    let data=await res.json();
-    console.log(data);
-    appendLanguages(data)
-}
-getLanguages()
-
-function appendLanguages(data) {
-
-    let parent=document.getElementById("languageList");
-
-    data.forEach((el)=> {
-        let options=document.createElement("option");
-        options.value=el.code;
-        options.innerText=el.name;
-        parent.append(options);
-    })
-
+function getInput() {
+    let input=document.getElementById("input_text").value;
+    return input;
 }
 
+function getInputLanguage() {
+    let input_language=document.getElementById("languageList1").value;
+    return input_language;
+}
+
+function getOutputLanguage() {
+    let output_language=document.getElementById("languageList").value;
+    localStorage.setItem(`lang`, JSON.stringify(output_language));
+    return output_language;
+}
+
+function displayOuptut(el) {
+    let output=document.getElementById("output_text");
+    output.innerHTML=null;
+    output.innerText=el;
+}
 
 
 
+async function main() {
 
+    let language=getInputLanguage();
+    if(language=="default") {
+        let lang= await detectLanguages();
+        translate(lang);
+    } else {
+        translate(language);
+    }
+
+
+}
+
+async function translate(lang) {
+    const res = await fetch("https://libretranslate.de/translate", {
+    method: "POST",
+    body: JSON.stringify({
+      q: getInput(),
+      source: lang,
+      target: getOutputLanguage(),
+    }),
+    headers: { "Content-Type": "application/json" },
+    });
+
+    let data = await res.json();
+    displayOuptut(data.translatedText)
+}
 
 async function detectLanguages() {
 
     let res = await fetch("https://libretranslate.de/detect", {
         method: "POST",
         body: JSON.stringify({
-          q: "hello",
+          q: getInput(),
         }),
         headers: { "Content-Type": "application/json" },
         });
     
         let data = await res.json();
-        console.log('data:', data.language);
-
-
+        let language = data[0].language;
+        return language;
 
 }
 
 
-
-
-async function run() {
-    
-    let input=document.getElementById("input_text").value;
-    let output=document.getElementById("output_text").value;
-
-    let input_language=document.getElementById("input_language");
-    let output_language=document.getElementById("languageList").value;
-
-    console.log(output_language);
+function run() {
+    console.log("one")
+    main();
    
 
 }
